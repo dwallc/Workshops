880dd568d97f5fe9de842c7b30dc4c86f96c2bd9
dwallc <desmond-wallace@uiowa.edu>
2018-03-13 14:05:31 -0500

    Included additional plotting

--- R/Model Interpretation and Visualization Using R III/Model Interpretation and Visualization Using R III.R
+++ R/Model Interpretation and Visualization Using R III/Model Interpretation and Visualization Using R III.R
@@ -24,15 +24,17 @@ setwd("E:/Desmond's Files/Cloud Storage/Dropbox/GitHub/Workshops/R/Model Interpr
 
 MIVdata02 <- import("./Data/MIVdata02.dta")
 
-## EStimate binomial regression model
+## Estimate binomial regression model
 
-model01 <- glm(fepres ~ children + as.factor(hsgrad),
+MIVdata02[["hsgrad"]] <- factor(MIVdata02[["hsgrad"]])
+
+model01 <- glm(fepres ~ children + hsgrad,
                family = "binomial",
                data = MIVdata02)
 
 summary(model01)
 
-## Calculate marginal effects
+## Calculate Average marginal effects
 
 summary(margins(model01))
 
@@ -41,3 +43,53 @@ summary(margins(model01,
 
 summary(margins(model01,
                 at = list(children = 8)))
+
+## Plotting Average Marginal Effects
+
+plot(margins(model01))
+
+## Plotting Predicted Probabilities and Marginal Effects
+
+cplot(model01,
+      x = "children",
+      what = "prediction",
+      main = "Predicted Probability of Supporting a Female President, Given Number of Children")
+
+cplot(model01,
+      x = "hsgrad",
+      what = "prediction",
+      main = "Predicted Probability of Supporting a Female President, Given High School Graduation")
+
+cplot(model01,
+      x = "children",
+      what = "effect",
+      main = "Average Marginal Effect of Number of Children")
+
+cplot(model01,
+      x = "hsgrad",
+      what = "effect",
+      main = "Average Marginal Effect of High School Graduation") # Does not work
+
+## Calculate Discrete Changes
+
+#### children Variable
+
+summary(margins(model01,
+                change = "dydx"))
+
+summary(margins(model01,
+                change = "sd"))
+
+summary(margins(model01,
+                change = "minmax"))
+
+summary(margins(model01,
+                change = "iqr"))
+
+summary(margins(model01,
+                change = c(1,
+                           2)))
+
+summary(margins(model01,
+                change = c(3,
+                           4))) # Change in marginal effect differs going from 3 -> 4 as opposed to 1 -> 2
