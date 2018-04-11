d977ba5c5653ea1bada722bf07d0a1fb6d2a7096
dwallc <desmond-wallace@uiowa.edu>
2018-04-10 22:45:09 -0500

    Included plots

--- Multilevel Modeling I/Multilevel Modeling using R I.R
+++ Multilevel Modeling I/Multilevel Modeling using R I.R
@@ -25,33 +25,156 @@ ipak <- function(pkg){
 packages <- c("tidyverse",
               "rio",
               "lme4",
-              "sjstats")
+              "sjstats",
+              "sjPlot")
 
 ipak(packages)
 
 ## Example I - Math Achievement
 
-### First, import the HSB All.xlsx dataset
+### Import the HSB All.xlsx dataset
 
 dataHSB <- import("./Data/HSB All R.xlsx")
 
-### Estimate Null Random Intercept Model
+### Visually explore the 'mathach' variable
 
-modelHSB <- lmer(mathach ~ 1 + (1|idGrp),
-                 data = dataHSB,
-                 REML = FALSE)
+#### Plot #1 - idGrp = 1224
 
-summary(modelHSB)
+plotHSB1 <- ggplot(data = dataHSB[dataHSB$idGrp == 1224, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point()
 
-icc(modelHSB)
+plotHSB1
 
-### Calculate GrandMean and Group-specific Means
+#### Plot #2 - idGrp = 1288
+
+plotHSB2 <- ggplot(data = dataHSB[dataHSB$idGrp == 1288, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point()
+
+plotHSB2
+
+#### Plot #3 - idGrp = 1296
+
+plotHSB3 <- ggplot(data = dataHSB[dataHSB$idGrp == 1296, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point()
+
+plotHSB3
+
+### Estimate the null model with only individual-level variation
+
+modelHSB1 <- lm(mathach ~ 1,
+                data = dataHSB) # Using ols
+
+summary(modelHSB1)
+
+### Calculate and plot the grand mean
 
 dataHSB <- dataHSB %>%
-           mutate(GrandMean = predict(modelHSB,
-                                      re.form = NA),
-                  idGrpMean = predict(modelHSB),
-                  idGrpEffect = idGrpMean - GrandMean)
+           mutate(GrandMean = predict(modelHSB1))
+
+#### Plot #4 - idGrp = 1224
+
+plotHSB4 <- ggplot(data = dataHSB[dataHSB$idGrp == 1224, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point() +
+            geom_hline(aes(yintercept = dataHSB$GrandMean[dataHSB$idGrp == 1224]))
+
+plotHSB4
+
+#### Plot #5 - idGrp = 1288
+
+plotHSB5 <- ggplot(data = dataHSB[dataHSB$idGrp == 1288, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point() +
+            geom_hline(aes(yintercept = dataHSB$GrandMean[dataHSB$idGrp == 1288]))
+
+plotHSB5
+
+#### Plot #6 - idGrp = 1296
+
+plotHSB6 <- ggplot(data = dataHSB[dataHSB$idGrp == 1296, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point() +
+            geom_hline(aes(yintercept = dataHSB$GrandMean[dataHSB$idGrp == 1296]))
+
+plotHSB6
+
+### Estimate the model with individual- and group-level variation
+
+modelHSB2 <- lmer(mathach ~ 1 + (1|idGrp),
+                  data = dataHSB,
+                  REML = FALSE)
+
+summary(modelHSB2)
+
+icc(modelHSB2)
+
+### Create data object featuring group-specific effects
+
+grpEffHSB <- ranef(modelHSB2)
+
+grpEffHSB <- rownames_to_column(grpEffHSB[["idGrp"]],
+                                var = "idGrp") %>%
+             mutate(idGrp = as.numeric(idGrp)) %>%
+             rename(idGrpEffect = `(Intercept)`)
+
+### Calculate and plot the group means
+
+dataHSB <- dataHSB %>%
+           mutate(GrandMean2 = predict(modelHSB,
+                                       re.form = NA),
+                  idGrpMean = predict(modelHSB2),
+                  idGrpEffect2 = idGrpMean - GrandMean2) %>%
+           inner_join(grpEffHSB,
+                      by = "idGrp")
+
+#### Plot #7 - idGrp = 1224
+
+plotHSB7 <- ggplot(data = dataHSB[dataHSB$idGrp == 1224, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point() +
+            geom_hline(aes(yintercept = dataHSB$GrandMean[dataHSB$idGrp == 1224])) +
+            geom_hline(aes(yintercept = dataHSB$idGrpMean[dataHSB$idGrp == 1224]),
+                       colour = "blue",
+                       linetype = "dashed")
+
+plotHSB7
+
+#### Plot #8 - idGrp = 1288
+
+plotHSB8 <- ggplot(data = dataHSB[dataHSB$idGrp == 1288, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point() +
+            geom_hline(aes(yintercept = dataHSB$GrandMean[dataHSB$idGrp == 1288])) +
+            geom_hline(aes(yintercept = dataHSB$idGrpMean[dataHSB$idGrp == 1288]),
+                       colour = "blue",
+                       linetype = "dashed")
+
+plotHSB8
+
+#### Plot #9 - idGrp = 1296
+
+plotHSB9 <- ggplot(data = dataHSB[dataHSB$idGrp == 1296, ],
+                   aes(x = idInd,
+                       y = mathach)) +
+            geom_point() +
+            geom_hline(aes(yintercept = dataHSB$GrandMean[dataHSB$idGrp == 1296])) +
+            geom_hline(aes(yintercept = dataHSB$idGrpMean[dataHSB$idGrp == 1296]),
+                       colour = "blue",
+                       linetype = "dashed")
+
+plotHSB9
+
 
 ## Example II - GSP
 
