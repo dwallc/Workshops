0f8e3fba1e1322abf269024da20e42693505a961
dwallc <desmond-wallace@uiowa.edu>
2018-10-07 05:50:50 -0500

    Finished plotting

--- R/Basic Network Analysis Using R/Basic Network Analysis Using R.R
+++ R/Basic Network Analysis Using R/Basic Network Analysis Using R.R
@@ -95,6 +95,7 @@ plot(exERl,
 title(main = "Example Network IV",
       sub = "100 Nodes, 40 Ties, Erdos-Renyi Random Graph Model, Undirected, Loops")
 
+
 # Load kracknets dataset
 
 data(kracknets,
@@ -106,268 +107,53 @@ str(krack_full_data_frame)
 
 View(krack_full_data_frame)
 
+## Create a igraph graph object from the data frame
 
-# Create Graphs
+krackFull <- graph.data.frame(krack_full_data_frame)
 
-## Bar Graph
+summary(krackFull) # reports basic properties and attributes of igraph object
 
-### Create an object containing number of days for each month (frequency count)
+## Print out the edge sequence for 'advice' network
 
-exDataMonthsCounts <- table(exData$Month)
+E(krackFull)$advice_tie
 
-### Create basic plot using "artist palette" approach (Add different plot layers separately)
+## Simplify the networks via removing loops
 
-# png("./bar.png") # Saving plot to specified file
+krackFull <- simplify(krackFull,
+                      remove.multiple = FALSE,
+                      remove.loops = TRUE)
 
-barplot(exDataMonthsCounts,
-        col = rainbow(5))
+## Plot the network
 
-title(main = "Number of Days in Each Month (May Through September)",
-      sub = "Source: airquality dataset",
-      xlab = "Months",
-      ylab = "Number of Days") # Add annotation layer
+### Assign different colors for each set of network ties
 
-legend(x = 7,
-       y = 30,
-       legend = names(exDataMonthsCounts),
-       fill = rainbow(5),
-       col = rainbow(5)) # Add legend layer
+plot(krackFull)
 
-# dev.off() # Turns graphics device off
+tieTypeColors = c(rgb(1,0,0,.5), # Advice Network
+                  rgb(0,0,1,.5), # Friendship Network
+                  rgb(0,0,0,.5)) # Report Network
 
-### Create same basic plot specifying aadditional layers within function
+E(krackFull)$color[E(krackFull)$advice_tie == 1] <- tieTypeColors[1]
 
-windows() # Opens a graphics device window
+E(krackFull)$color[E(krackFull)$friendship_tie == 1 ] <- tieTypeColors[2]
 
-barplot(exDataMonthsCounts,
-        main = "Number of Days in Each Month (May Through September)",
-        sub = "Source: airquality dataset",
-        xlab = "Months",
-        ylab = "Number of Days",
-        col = rainbow(5),
-        legend = names(exDataMonthsCounts))
+E(krackFull)$color[E(krackFull)$reports_to_tie == 1] <- tieTypeColors[3]
 
-### Create horizontal version
+E(krackFull)$arrow.size <- 0.5
 
-barplot(exDataMonthsCounts,
-        main = "Number of Days in Each Month (May Through September)",
-        sub = "Source: airquality dataset",
-        xlab = "Months",
-        ylab = "Number of Days",
-        col = rainbow(5),
-        legend = names(exDataMonthsCounts),
-        horiz = TRUE)
+plot(krackFull,
+     vertex.color = "red",
+     vertex.label = NA,
+     edge.arrow.size = 0.5)
 
-## Box Plot
+legend(x = 1.5,
+       y = 1.25,
+       legend = c('Advice',
+                  'Friendship',
+                  'Reports To'),
+       col = tieTypeColors,
+       lty = 1,
+       cex = 1)
 
-### Create boxplot for all recorded temperatures
-
-boxplot(exData$Temp,
-        main = "Temperature for May - September '73",
-        sub = "Source: airquality dataset",
-        ylab = "Temperature",
-        col = "blue")
-
-### Create boxplots for each month's recorded temperatures
-
-boxplot(Temp ~ Month,
-        data = exData,
-        main = "Temperature for May - September '73",
-        sub = "Source: airquality dataset",
-        xlab = "Months",
-        ylab = "Temperature",
-        col = "green")
-
-## Histogram
-
-### Create a histogram of Temperature frequencies
-
-hist(exData$Temp,
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "gray")
-
-### Create a histogram of Temperature frequencies for each month
-
-par(mfrow = c(3,
-              2)) # Add additional plots by row
-
-hist(exData$Temp[exData$Month == 5],
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances in May",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "red")
-
-hist(exData$Temp[exData$Month == 6],
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances in June",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "yellow")
-
-hist(exData$Temp[exData$Month == 7],
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances in July",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "green")
-
-hist(exData$Temp[exData$Month == 8],
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances in August",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "blue")
-
-hist(exData$Temp[exData$Month == 9],
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances in September",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "purple")
-
-hist(exData$Temp,
-     freq = TRUE,
-     main = "Histogram of Temperature Occurances",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Frequency",
-     col = "gray")
-
-## Kernel Density
-
-### Create a kernel density plot for observed Wind
-
-par(mfrow = c(1,
-              1))
-
-plot(density(exData$Wind),
-     main = "Kernel Density of Observed Wind",
-     col = "black",
-     lwd = 3,
-     zero.line = FALSE)
-
-### Create a kernel density plot for observed Wind for each month
-
-par(mfrow = c(3,
-              2))
-
-plot(density(exData$Wind[exData$Month == 5]),
-     main = "Kernel Density of Observed Wind in May",
-     col = "red",
-     lwd = 3,
-     zero.line = FALSE)
-
-plot(density(exData$Wind[exData$Month == 6]),
-     main = "Kernel Density of Observed Wind in June",
-     col = "yellow",
-     lwd = 3,
-     zero.line = FALSE)
-
-plot(density(exData$Wind[exData$Month == 7]),
-     main = "Kernel Density of Observed Wind in July",
-     col = "green",
-     lwd = 3,
-     zero.line = FALSE)
-
-plot(density(exData$Wind[exData$Month == 8]),
-     main = "Kernel Density of Observed Wind in August",
-     col = "blue",
-     lwd = 3,
-     zero.line = FALSE)
-
-plot(density(exData$Wind[exData$Month == 9]),
-     main = "Kernel Density of Observed Wind in September",
-     col = "purple",
-     lwd = 3,
-     zero.line = FALSE)
-
-plot(density(exData$Wind),
-     main = "Kernel Density of Observed Wind",
-     col = "black",
-     lwd = 3,
-     zero.line = FALSE)
-
-## Scatterplot
-
-### Create a scatterplot plotting the relationship between Wind and Temperature
-
-plot(exData$Temp,
-     exData$Wind,
-     main = "Relationship between Temperature and Wind",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Wind",
-     pch = 19)
-
-### Add a line of best fit
-
-abline(lm(Wind ~ Temp,
-          data = exData),
-       col = "blue")
-
-### Distinguish scatterplot by Month
-
-plot(exData$Temp,
-     exData$Wind,
-     main = "Relationship between Temperature and Wind",
-     sub = "Source: airquality dataset",
-     xlab = "Temperature",
-     ylab = "Wind",
-     pch = 19,
-     col = exData$Month)
-
-legend(x = 60,
-       y = 8,
-       legend = unique(exData$Month),
-       col = unique(exData$Month),
-       fill = unique(exData$Month))
-
-abline(lm(Wind ~ Temp,
-          data = exData),
-       col = "blue",
-       lwd = 4)
-
-abline(lm(Wind ~ Temp,
-          data = exData,
-          subset = Month == 5),
-       col = "cyan",
-       lwd = 2,
-       lty = 2)
-
-abline(lm(Wind ~ Temp,
-          data = exData,
-          subset = Month == 6),
-       col = "magenta",
-       lwd = 2,
-       lty = 2)
-
-abline(lm(Wind ~ Temp,
-          data = exData,
-          subset = Month == 7),
-       col = "yellow",
-       lwd = 2,
-       lty = 2)
-
-abline(lm(Wind ~ Temp,
-          data = exData,
-          subset = Month == 8),
-       col = "gray",
-       lwd = 2,
-       lty = 2)
-
-abline(lm(Wind ~ Temp,
-          data = exData,
-          subset = Month == 9),
-       col = "black",
-       lwd = 2,
-       lty = 2)
+title(main = "Krackhardt's Data on Ties among High-Tech Workers",
+      sub = "Advice, Friendship, and Reports To Networks")
